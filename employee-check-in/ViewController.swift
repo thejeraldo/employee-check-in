b74957d0f0baadadbaf3c70c3fa8b3033eae546e
Jerald Abille <thejeraldo@gmail.com>
2024-10-30 20:02:32 +0800

    first commit

---
+++ employee-check-in/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  employee-check-in
+//
+//  Created by Jerald Allen Abille on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ employee-check-inUITests/employee_check_inUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  employee_check_inUITestsLaunchTests.swift
+//  employee-check-inUITests
+//
+//  Created by Jerald Allen Abille on 10/30/24.
+//
+
+import XCTest
+
+final class employee_check_inUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
