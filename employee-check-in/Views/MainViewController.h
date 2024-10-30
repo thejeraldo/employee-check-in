779c4479d3fc75186a87c668c9a80f027ef4d651
Jerald Abille <thejeraldo@gmail.com>
2024-10-30 20:09:30 +0800

    Added MainViewController and set it as initial view controller

---
+++ employee-check-in/Views/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  employee-check-in
+//
+//  Created by Jerald Allen Abille on 10/30/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ employee-check-in/Views/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  employee-check-in
+//
+//  Created by Jerald Allen Abille on 10/30/24.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"MainViewController");
+}
+
+@end
