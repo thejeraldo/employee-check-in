09464a49a8b043e083358e9b98afc00c8ee32dea
Jerald Abille <thejeraldo@gmail.com>
2024-10-30 20:26:05 +0800

    Added startButton in MainViewController

--- employee-check-in/Views/MainViewController.m
+++ employee-check-in/Views/MainViewController.m
@@ -16,7 +16,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"MainViewController");
+    
+    [self setupViews];
+}
+
+- (void)setupViews {
+    [self.view setBackgroundColor:[UIColor systemBackgroundColor]];
+    
+    UIButton *startButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [startButton setTitle:@"Start" forState:UIControlStateNormal];
+    [startButton setBackgroundColor:[UIColor tertiarySystemFillColor]];
+    [startButton setTitleColor:[UIColor labelColor] forState:UIControlStateNormal];
+    [[startButton layer] setCornerRadius:10];
+    [startButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [[self view] addSubview:startButton];
+    [NSLayoutConstraint activateConstraints:@[
+        [startButton.leadingAnchor constraintEqualToAnchor:self.view.layoutMarginsGuide.leadingAnchor],
+        [startButton.trailingAnchor constraintEqualToAnchor:self.view.layoutMarginsGuide.trailingAnchor],
+        [startButton.heightAnchor constraintEqualToConstant: 44],
+        [startButton.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor]
+    ]];
+    
+    [startButton addTarget:self action:@selector(didTapStartButton) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)didTapStartButton {
+    NSLog(@"Start!");
 }
 
 @end
